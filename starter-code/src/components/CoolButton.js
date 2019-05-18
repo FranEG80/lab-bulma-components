c5fec2228a719ff6acbbfb31c4903da93a7b6827
Fernando Lacaba <fernando.lacaba.herrera@gmail.com>
2019-05-18 13:56:04 +0200

    Iteration 3 completed

---
+++ starter-code/src/components/CoolButton.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const obj = {
+  isActive: 'is-active',
+  isBlack: 'is-black',
+  isCentered: 'is-centered',
+  isDanger: 'is-danger',
+  isDark: 'is-dark',
+  isFocused: 'is-focused',
+  isGrouped: 'is-grouped',
+  isHovered: 'is-hovered',
+  isInfo: 'is-info',
+  isInverted: 'is-inverted',
+  isLarge: 'is-large',
+  isLight: 'is-light',
+  isLink: 'is-link',
+  isLoading: 'is-loading',
+  isMedium: 'is-medium',
+  isOutlined: 'is-outlined',
+  isPrimary: 'is-primary',
+  isRight: 'is-right',
+  isRounded: 'is-rounded',
+  isSelected: 'is-selected',
+  isSmall: 'is-small',
+  isStatic: 'is-static',
+  isSuccess: 'is-success',
+  isText: 'is-text',
+  isWarning: 'is-warning',
+  isWhite: 'is-white',
+}
+
+const CoolButton = (props) => {
+  const keys = Object.keys(obj)
+  let att = ''
+  Object.keys(props).forEach(key => {
+    keys.includes(key) ? att +=` ${obj[key]}`:null
+  })
+
+  const {extraClass} = props;
+
+   return (
+    <button className={`button${att} ${extraClass}`}>{props.children}</button>
+   )
+}
+
+export default CoolButton
